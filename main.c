5a0552c2a199df61f3fe042d3a8be2c0ec924bd0
Franz Knipp <franz@qnipp.com>
2020-02-03 01:59:57 +0100

    Add an interation counter for profiling

--- main.c
+++ main.c
@@ -10,27 +10,30 @@ int main()
 
   int n = 0;
 
-  for (int y = 0; y < 200; y++)
+  for (int i = 0; i < 10000; i++)
   {
-    for (int m = 0; m <= 11; m++)
+    for (int y = 0; y < 200; y++)
     {
-      for (int d = 1; d <= 28; d++)
+      for (int m = 0; m <= 11; m++)
       {
-        int knipp = getDaysFromYMD_knipp(y, m, d);
-        int mktime = getDaysFromYMD_mktime(y, m, d);
-        int v8 = getDaysFromYMD_v8(y + 1900, m, d);
-        int libreOffice = getDaysFromYMD_libreOffice(y + 1900, m + 1, d);
-
-        if (knipp != mktime || mktime != v8 || v8 != libreOffice)
+        for (int d = 1; d <= 28; d++)
         {
-          printf("%04d-%02d-%02d\n", y + 1900, m, d);
-          printf("knipp:       %d\n", knipp);
-          printf("mktime:      %d\n", mktime);
-          printf("v8:          %d\n", v8);
-          printf("libreOffice: %d\n", libreOffice);
-        }
+          int libreOffice = getDaysFromYMD_libreOffice(y + 1900, m + 1, d);
+          int knipp = getDaysFromYMD_knipp(y, m, d);
+          int mktime = getDaysFromYMD_mktime(y, m, d);
+          int v8 = getDaysFromYMD_v8(y + 1900, m, d);
 
-        n++;
+          if (knipp != mktime || mktime != v8 || v8 != libreOffice)
+          {
+            printf("%04d-%02d-%02d\n", y + 1900, m, d);
+            printf("knipp:       %d\n", knipp);
+            printf("mktime:      %d\n", mktime);
+            printf("v8:          %d\n", v8);
+            printf("libreOffice: %d\n", libreOffice);
+          }
+
+          n++;
+        }
       }
     }
   }
